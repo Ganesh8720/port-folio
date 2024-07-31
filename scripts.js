9c20870e64fdffc6e4ee14e872e12ed58801fcdf
Kondigatti Ganesh Kumar <154490510+Ganesh8720@users.noreply.github.com>
2024-07-31 20:44:41 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutMeSection = document.querySelector('.about-me');
+    aboutMeSection.style.opacity = '1';
+    aboutMeSection.style.transform = 'translateY(0)';
+});
